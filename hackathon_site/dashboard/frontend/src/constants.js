3ea0de31432f42d000d61a42bb593279c71294ca
guaaaaa <63753679+guaaaaa@users.noreply.github.com>
2025-04-10 11:41:57 -0400

    Update constants.js

--- hackathon_site/dashboard/frontend/src/constants.js
+++ hackathon_site/dashboard/frontend/src/constants.js
@@ -6,3 +6,4 @@ export const hardwareSignOutStartDate = new Date(2020, 9, 1, 23, 59);
 export const hardwareSignOutEndDate = new Date(2025, 9, 30, 11, 59);
 export const hssTestUserGroup = "HSS Test Users";
 export const minProjectDescriptionLength = 60;
+export const server_url = "https://newhacks.ca";
